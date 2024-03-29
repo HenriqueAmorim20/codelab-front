8d0632a4fe3bca04f27db36a9e6b5c01424f7eaa
Henrique Melo <hacmelo@gmail.com>
2024-03-29 17:36:01 -0300

    altera prefixo seletor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cl-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'codelab-front';
-}
+export class AppComponent {}
