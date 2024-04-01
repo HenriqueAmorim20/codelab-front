f31d70277a55854cce56d1d10bab868ff81f5c45
Henrique Melo <hacmelo@gmail.com>
2024-04-01 18:58:50 -0300

    stand alone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cl-root',
-  // standalone: true,
-  // imports: [],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
