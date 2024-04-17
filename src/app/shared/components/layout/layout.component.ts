62e56367ee4e9cbf275b8ea5822438634136b27d
Henrique Melo <hacmelo@gmail.com>
2024-04-17 15:50:08 -0300

    adiciona separacao dos componentes

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/menu.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'cl-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MenuComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
