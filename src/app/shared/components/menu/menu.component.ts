957efe8be391d0b39e050b5028ef8a6e90177c01
Henrique Melo <hacmelo@gmail.com>
2024-04-17 17:31:35 -0300

    adiciona menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,10 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+interface IMenuItem {
+  label: string;
+  icon: string;
+  path: string;
+}
 
 @Component({
   selector: 'cl-menu',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  menuItems: IMenuItem[] = [
+    {
+      label: 'Home',
+      icon: 'home',
+      path: '/',
+    },
+  ];
+
+  handleNavigation(path: string): void {
+    console.log(path);
+  }
+}
