f620ed46f09f03f338d912ce87b7c6d5d7b26146
Henrique Melo <hacmelo@gmail.com>
2024-04-17 15:39:13 -0300

    adiciona estrutura inicial da aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
 @Component({
   selector: 'cl-root',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LayoutComponent, HomeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/shared/components/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cl-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
