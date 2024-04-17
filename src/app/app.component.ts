ba2f0bde5e7e820672bf5da3d5121d51fa010675
Henrique Melo <hacmelo@gmail.com>
2024-04-17 14:13:19 -0300

    altera estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'cl-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [LoginComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
