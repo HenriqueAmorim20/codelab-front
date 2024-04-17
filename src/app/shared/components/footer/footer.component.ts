19fe953d882ef025045ed7c2ad5ec050dcc5b1a1
Henrique Melo <hacmelo@gmail.com>
2024-04-17 16:37:32 -0300

    adiciona footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { version } from '../../../../../package.json';
 
 @Component({
   selector: 'cl-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  today = new Date();
+  year = this.today.getFullYear();
+  version = version;
+}
