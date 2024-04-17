6dc09bd994c16c32f3b9f582d658f694ac5853b2
Henrique Melo <hacmelo@gmail.com>
2024-04-17 17:14:25 -0300

    adiciona header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'cl-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  logout(): void {
+    console.log('logout');
+  }
+}
